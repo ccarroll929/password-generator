4f1a960f243ccf3c2d8b5b4f33a3f5974d3a4c8b
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2023-12-07 17:02:16 -0600

    added new code

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Password Generator project:
+# Password Generator Project:
 
-## Description: In this project I modified starter code in order to create a random password generator application in the browser that will create unique passwords for employees based on the criteria they select. I want the app to have a responsive user interface that adapts to multiple screen sizes for optimal user-friendliness. 
+## Description: In this project I added to the JavaScript starter code in order to create a random password generator application that will create unique passwords for users based on the criteria they select. I utilized multiple prompts in order to acquire user input and then created a function that will generate a random password that meets their chosen criteria.
 
-## Screenshot of Deployed Application: 
+## Screenshot of Deployed Application:
 
-## Link to Deployed Application: 
+
+## Link to Deployed Application:
 
 ## Installation: [Visual Studio Code](https://code.visualstudio.com/download)
-##               [Git for Windows](https://gitforwindows.org/) 
+##               [Git for Windows](https://gitforwindows.org/)
 
 
+## Acknowledgments/Credit: [Daniel Vega](https://www.youtube.com/@danielvega545)
--- script.js
+++ script.js
@@ -1,14 +1,59 @@
+// New code: Added variables for password character types
+let length = 8; 
+let allChars = [];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numeric = ['0','1','2','3','4','5','6','7','8','9'];
+let specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '?', '<', '>']
+
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+ 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  getPrompts();
+  password = generatePassword(); 
+  let passwordText = document.querySelector("#password");
+  passwordText.value= password; 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+ // New Code: Added the generatePassword function with for loop  
+function generatePassword() {
+  let password = " "; 
+  for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * allChars.length);
+      password = password + allChars[randomIndex]; 
+  } 
+  return password;
+
+}
+  
+  //New code: Prompts for character length/types. 
+function getPrompts() {
+  allChars = [];
+  length = parseInt(prompt("How many characters do you want the password to have? Choose a number between 8 and 128"));
+  
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Error: Enter a valid number between 8 and 128"); 
+    return false; 
+  }
+  if (confirm("Do you want your password to contain lowercase letters?")) {
+    allChars = allChars.concat(lowerCase);
+  }
+  if (confirm("Do you want your password to contain upper letters?")) {
+    allChars = allChars.concat(upperCase);
+  }
+  if (confirm("Do you want your password to contain numbers?")) {
+      allChars = allChars.concat(numeric);
+    }
+  if (confirm("Do you want your password to contain symbols?")) {
+      allChars = allChars.concat(specialChars);
+    }
+  return true; 
+  }
+
